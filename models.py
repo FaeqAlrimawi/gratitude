a82d74a5e47dba17ae074283b9a1fd7adbbd1d23
FaeqAlrimawi <faeq.rimawi@gmail.com>
2022-12-05 12:40:02 +0000

    up

--- models.py
+++ models.py
@@ -16,7 +16,13 @@ class ActorType(enum.Enum):
     GIVER = "GIVER"
     RECEIVER = "RECEIVER"
     
-         
+# an auxiliary table: only foreign keys; created without association model class. 
+act_actors = db.Table('act_actors',
+                   db.Column('act_id', db.Integer, db.ForeignKey('gratitudeact.id')),
+                   db.Column('giver_id', db.Integer, db.ForeignKey('actor.id')),
+                   db.Column('receiver_id', db.Integer, db.ForeignKey('actor.id'))
+)
+           
 class Actofkindness(db.Model):
     __name__="aok"
     id = db.Column(db.Integer, primary_key=True)
@@ -29,12 +35,17 @@ class Gratitudeact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     popularity = db.Column(db.Enum(Level))
     gratitudeMessage = db.Column(db.String(1000))
-    actor = db.Column(db.Integer, db.ForeignKey('actor.id'))
+    # actor = db.Column(db.Integer, db.ForeignKey('actor.id'))
     context =  db.Column(db.Integer, db.ForeignKey('context.id'))
     prompts = db.relationship('Prompt')
-   
+    act_actors = db.relationship(
+                                 'Gratitudeact',
+                                 secondary=act_actors,
+                                 primaryjoin=(act_actors.c.act_id == id),
+                                 backref = db.backref('other_people_rating', lazy='dynamic'), lazy='dynamic'
+                                 ) 
     
-    
+ 
 # this can be used to raise motivation for actors by telling them how gratitude (and kindness in general) can positively impact others and them    
 class GratitudeImpactInfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -48,8 +59,15 @@ class Actor(db.Model):
       motivation = db.Column(db.Enum(Level))
       Ability = db.Column(db.Enum(Level)) 
       type = db.Column(db.Enum(ActorType))
-      acts = db.relationship('Gratitudeact')
-      user = db.Column(db.Integer, db.ForeignKey('user.id'))       
+    #   acts = db.relationship('Gratitudeact')
+      user = db.Column(db.Integer, db.ForeignKey('user.id'))      
+      act_actors =  db.relationship(
+                                 'Actor',
+                                 secondary=act_actors,
+                                 primaryjoin=(act_actors.c.giver_id == id),
+                                 secondaryjoin=(act_actors.c.receiver_id == id),
+                                 backref = db.backref('other_people_rating', lazy='dynamic'), lazy='dynamic'
+                                 ) 
       
 # this class characterises the context for an act... for now it just specifies time, but later can include other parameters such as space, equipment
 class Context(db.Model):
