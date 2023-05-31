bfe8b9032e7e2d5425e968deef4bd9a3c44671e1
ase2023 <ase2023.kindness@gmail.com>
2023-05-31 11:36:16 +0100

    done testing

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ app = create_app()
 if __name__ == '__main__':
     # app.run(debug=True)
     
-    def test():
-        # breakpoint()
-        print("testing", 0/0)
+    # def test():
+    #     # breakpoint()
+    #     print("testing", 0/0)
         
         
-    test()
-    test()
-    pdb.pm()
+    # test()
+   
+    # pdb.pm()
         
     
 
